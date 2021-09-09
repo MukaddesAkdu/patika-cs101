6f4661c1ce363795ecd86923cb476043bd580283
MukaddesAkdu <76842969+MukaddesAkdu@users.noreply.github.com>
2021-09-09 18:58:50 +0300

    Add files via upload

---
+++ patika-C#101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace patika_C_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
